a87d7ac61719aa2be04ed8063aa655692bd584ac
Ilja work <i.meleshkevich@gmail.com>
2020-10-14 14:43:00 +0200

    Initial project structure

---
+++ src/components/storepicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import  {render} from 'react-dom';
+class StorePicker extends React.Component {
+    render() {
+        return <form className='store-selector'>
+            <h2>Please Enter a Store</h2>
+        </form> 
+    }
+}
+export default StorePicker;
