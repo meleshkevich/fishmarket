fe7883aceb7d54d02a0c421a38598ca6a672acb6
Ilja work <i.meleshkevich@gmail.com>
2020-10-15 12:27:41 +0200

    adding React Router

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import  {render} from 'react-dom';
 import App from './components/App';
 import StorePicker from './components/StorePicker';  
- 
- render(<App/>, document.getElementById('main'));
+import Router from './components/Router';
+import { Route } from 'react-router-dom';
+
+ render(<Router/>, document.getElementById('main'));
