996eb0f11f622ef1ca2c9e66e10805f345a61ed3
Ilja work <i.meleshkevich@gmail.com>
2020-10-15 15:45:55 +0200

    adding onClick changing page

--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -3,11 +3,26 @@ import {getFunName} from'../helpers';
 import '../css/style.css';
 
 class StorePicker extends React.Component {
-  render() {
+    myImput = React.createRef();
+
+    goToStore = event => {
+        // stop form from default submitting
+        event.preventDefault();
+        // get text from input form
+        const storeName = this.myImput.current.value;
+        // change page to /store/whatever-was-entered
+        this.props.history.push(`/store/${storeName}`);
+    };
+
+    render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" 
+        ref={this.myImput}
+        required placeholder="Store Name" 
+        defaultValue={getFunName()} 
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import  {render} from 'react-dom';
-import App from './components/App';
-import StorePicker from './components/StorePicker';  
 import Router from './components/Router';
-import { Route } from 'react-router-dom';
 
  render(<Router/>, document.getElementById('main'));
